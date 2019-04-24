e63d2d2449b807cb9ca917596780035121d44fc8
songyanyan <songyanyan@openpubu.com>
2019-04-24 10:31:45 +0800

    axios cross success

--- src/App.vue
+++ src/App.vue
@@ -6,8 +6,25 @@
 </template>
 
 <script>
+import axios from 'axios'
 export default {
-  name: 'App'
+  name: 'App',
+  mounted () {
+    this.getToken()
+  },
+  methods: {
+    getToken () {
+      let grantType = 'client_credential'
+      let appid = 'wx572cd97544c5dfea'
+      let secret = '3bca7c37af5e40c073807026d7bedac5'
+      let url = '/api/cgi-bin/token?grant_type=' + grantType + '&appid=' + appid + '&secret=' + secret
+      axios.get(url).then(res => {
+        console.log(res)
+      }).catch(err => {
+        console.log(err)
+      })
+    }
+  }
 }
 </script>
 
